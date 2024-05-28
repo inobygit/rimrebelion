ee6a716a33c91d2ca510e2f0fa089d80cc65f711
b.plavy <branislav.plavy@gmail.com>
2024-05-28 10:24:03 +0200

    email templates

---
+++ woocommerce/emails/email-mobile-messaging.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Email mobile messaging
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-mobile-messaging.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 7.0
+ */
+
+
+echo '<p>'.__('In case of any questions, do not hasitate to contact us by e-mail: ', 'rimrebellion').' <a href="mailto:help@rimrebellion.com">'. __("help@rimrebellion.com ", 'rimrebellion') .' </a> '.__("or by phone: ", 'rimrebellion').' <a href="tel:+421915111199">'. __("+ 421 915 111 199.", 'rimrebellion') .' </a></p>';
+echo '<p><b>'.__("Thank you for shopping at ").' <a href="https://rimrebelion.com">'.__("rimrebelion.com!", 'rimrebellion').'</a></b></p>';
