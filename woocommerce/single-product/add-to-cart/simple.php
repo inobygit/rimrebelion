0026c34065b2e17b7303f285be89d110458c0262
b.plavy <branislav.plavy@gmail.com>
2024-09-04 13:34:19 +0200

    remove quantity input

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -90,19 +90,6 @@ if ( $product->is_in_stock() ) : ?>
     </div>
     <?php } ?>
     <div class="simple-row">
-        <?php
-			do_action( 'woocommerce_before_add_to_cart_quantity' );
-	
-			woocommerce_quantity_input(
-				array(
-					'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-					'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-					'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-				)
-			);
-	
-			do_action( 'woocommerce_after_add_to_cart_quantity' );
-			?>
         <?php rebellion_add_to_cart_btn(
 				array(
 					'text' => esc_html( $product->single_add_to_cart_text() ),
