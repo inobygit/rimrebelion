ada1a6d88a95d0ec68628a96b8e90bbaa83ddf23
b.plavy <branislav.plavy@gmail.com>
2024-10-31 10:21:39 +0100

    prices update

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -22,23 +22,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 
 $lowest_price = null;
+$has_different_prices = false; // Track if there are different prices
+
 if ($product->is_type('variable')) {
+    $available_variations = $product->get_available_variations();
+    $first_price = null; // Store the first price for comparison
 
-
-$available_variations = $product->get_available_variations();
-foreach ($available_variations as $variation) {
-    $price = $variation['display_price'];
-    if ($lowest_price === null || $price < $lowest_price) {
-        $lowest_price = $price;
+    foreach ($available_variations as $variation) {
+        $price = $variation['display_price'];
+        if ($lowest_price === null || $price < $lowest_price) {
+            $lowest_price = $price;
+        }
+        // Check if the price is different from the first price
+        if ($first_price === null) {
+            $first_price = $price;
+        } elseif ($price !== $first_price) {
+            $has_different_prices = true; // Set flag if prices differ
+        }
     }
-}
-$lowest_price = number_format($lowest_price, 2);
+    $lowest_price = number_format($lowest_price, 2, ',', ' ');
 
-$priceHtml = '<span class="woocommerce-Price-amount amount"><bdi>'. __("from ", 'inoby') .''. $lowest_price .'&nbsp;<span class="woocommerce-Price-currencySymbol">€</span></bdi></span>';
+    // Update priceHtml only if there are different prices
+    if ($has_different_prices) {
+        $priceHtml = '<span class="woocommerce-Price-amount amount"><bdi>'. __("from ", 'inoby') .''. $lowest_price .'&nbsp;<span class="woocommerce-Price-currencySymbol">€</span></bdi></span>';
+    } 
 }
-
 
 ?>
 <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>">
-    <?= ($lowest_price && $lowest_price != 0 ? $priceHtml : $product->get_price_html()) ?>
+    <?= (isset($priceHtml) && $lowest_price != 0 ? $priceHtml : $product->get_price_html()) ?>
 </p>
