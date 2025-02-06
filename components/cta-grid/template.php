094f205236af2d1f807d9867bde7e3e0b431ff9c
b.plavy <branislav.plavy@gmail.com>
2025-02-06 11:29:51 +0100

    fix grid

--- components/cta-grid/template.php
+++ components/cta-grid/template.php
@@ -26,9 +26,8 @@ $cta_grid = mb_get_block_field("cta-grid");
         <?php if(!empty($cta_grid)) { ?>
         <div class="row">
             <div class="col-12">
-                <?php 
-                echo '<div class="cta-grid-wrap-inner">';
-
+                <div class="cta-grid-wrap-inner size-<?= sizeof($cta_grid) ?>">
+                    <?php
                 foreach ($cta_grid as $i => $cta) {
                   $cta_url = $cta["url"] ?? "";
                   if (isset($cta["bg"])) {
@@ -42,32 +41,33 @@ $cta_grid = mb_get_block_field("cta-grid");
                   echo isset($cta["headline"]) ? "<h2>" . $cta["headline"] . "</h2>" : ""; 
                   if(isset($cta['text'])){
                   ?>
-                <p class='link'>
-                    <?= $cta['text'] ?>
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.5 7.25H14.5V8.75H0.5V7.25Z" fill="white" />
-                        <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M13.3853 7.99617L10.4194 5.03033L11.4801 3.96967L15.5143 8.00382L11.4763 11.9841L10.4233 10.9159L13.3853 7.99617Z"
-                            fill="white" />
-                    </svg>
+                    <p class='link'>
+                        <?= $cta['text'] ?>
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.5 7.25H14.5V8.75H0.5V7.25Z"
+                                fill="white" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M13.3853 7.99617L10.4194 5.03033L11.4801 3.96967L15.5143 8.00382L11.4763 11.9841L10.4233 10.9159L13.3853 7.99617Z"
+                                fill="white" />
+                        </svg>
 
-                </p>
-                <?php }
+                    </p>
+                    <?php }
                   echo "</a>";
                 }
                 echo "</div>";
             ?>
+                </div>
+            </div>
+            <?php } ?>
+            <div class="row items-center">
+                <div class="col-9 col-md-12">
+                    <?php if(!empty($blockBtnText) && !empty($blockBtnUrl)){ ?>
+                    <a href="<?= $blockBtnUrl ?>" class="button triangleBoth black">
+                        <?= $blockBtnText ?>
+                    </a>
+                    <?php } ?>
+                </div>
             </div>
         </div>
-        <?php } ?>
-        <div class="row items-center">
-            <div class="col-9 col-md-12">
-                <?php if(!empty($blockBtnText) && !empty($blockBtnUrl)){ ?>
-                <a href="<?= $blockBtnUrl ?>" class="button triangleBoth black">
-                    <?= $blockBtnText ?>
-                </a>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
 </section>
